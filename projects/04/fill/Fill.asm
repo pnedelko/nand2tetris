feb08ee001dac146f10726e547399be923b902dd
Pavel Nedelko <pnedelko@gmail.com>
2019-11-28 21:42:11 +0100

    Fill & Mult

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,69 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//initial offset = -1
+@offset
+M=-1
+
+//max offset = 8192-1 (don't ask why)
+@8191
+D=A
+@moffset
+M=D
+
+(LOOP)
+    @KBD
+    D=M
+
+    //if a key is pressed - write black pixels
+    @BLACK
+    D;JGT
+
+    //else write white pixels
+    @WHITE
+    0;JMP
+
+(BLACK)
+    // if offset == maxOffset -> return
+    @offset
+    D=M
+    @moffset
+    D=M-D
+    @LOOP
+    D;JEQ
+
+    //increment offset
+    @offset
+    M=M+1
+
+    //make all pixels at the current offset black
+    @offset
+    D=M
+    @SCREEN
+    A=D+A // D + offset
+    M=M-1 //set pixel to black
+
+    @LOOP
+    0;JMP
+
+(WHITE)
+    //if offset is < 0, then we are done
+    @offset
+    D=M
+    @LOOP
+    D;JLT
+
+    //make all pixels at the current offset white
+    @offset
+    D=M
+    @SCREEN
+    A=D+A // D + offset
+    M=0 //set pixel to white
+
+    //decrement offset
+    @offset
+    M=M-1
+
+    @LOOP
+    0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,34 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+@R1
+D=M //D=R1
+@i
+M=D //i=R1
+@R2
+M=0 //R2=0
+
+(LOOP)
+    @i
+    D=M // D=i
+    @END
+    D;JEQ // if i==0 goto END
+
+    // sum
+    @R0
+    D=M // D=R0
+    @R2
+    M=D+M // R2 = R2 + R0
+
+    // decrement i
+    @i
+    M=M-1
+
+    // loop
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
